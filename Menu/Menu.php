89d0f2e6710b30fcfa9937001b99bd71e88de5fc
yintii <keleheart1@gmail.com>
2023-03-27 16:54:23 -0700

    got the plugin to upload the form submission successfully

--- Menu/Menu.php
+++ Menu/Menu.php
@@ -1,8 +1,113 @@
 <?php
+
+
+        function upload_doc(){
+            require_once( dirname(__FILE__) . '/../../../../wp-load.php' );
+
+            // it allows us to use wp_handle_upload() function
+            require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+            $target_dir = content_url();
+            $target_file = $target_dir . basename($_FILES["fileData"]["name"]);
+            $uploadOk = 1;
+            $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+            // Check if file already exists
+            if (file_exists($target_file)) {
+                echo "Sorry, file already exists.";
+                echo "<br />";
+                $uploadOk = 0;
+            }
+
+            // Check file size
+            if ($_FILES["fileData"]["size"] > 1500000) {
+                echo "Sorry, your file is too large.";
+                echo "<br />";
+                $uploadOk = 0;
+            }
+
+            // Allow certain file formats
+            if($fileType !== "docx" && $fileType !== "pdf") {
+                if($fileType == null){
+                return;
+                }else{
+                echo "Sorry, docx and pdf files are allowed.";
+                echo "<br />";
+                $uploadOk = 0;
+                }
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if($fileType === "pdf"){
+
+                }
+                $upload = wp_upload_bits($_FILES["fileData"]["name"], null, file_get_contents( $_FILES["fileData"]["tmp_name"]));
+
+                if ($upload) {
+                return $upload['url'];
+                } else {
+                echo "Sorry, there was an error uploading your file.";
+                echo "<br />";
+                return;
+                }
+            }
+        }
+
+    function writeData(
+        $date_time,
+        $location,
+        $meeting_type,
+        $file_type,
+        $file_url
+    ){
+
+        global $wpdb;
+
+        $table = $wpdb->prefix . 'meetings';
+
+        $data = array(
+            'time' => $date_time,
+            'location' => $location,
+            'meeting_type' => $meeting_type,
+            'file_type' => $file_type,
+            'file_path' => $file_url
+        );
+
+        $format = array(
+            '%s', #datetime
+            '%s', #location
+            '%s', #meeting_type
+            '%s', #file_type
+            '%s', #file_url
+        );
+
+       try {
+         $wpdb->insert($table, $data, $format);
+       } catch (Exception $e) {
+            echo 'There was an error trying to insert the data to the database' . $e->getMessage();
+       }
+
+    }
+
+
+
     if(isset($_POST['submit'])){
         echo "Submit detected!";
-        print_r($_POST);
-        print_r($_FILES);
+
+        $upload_url = upload_doc($_FILES['fileData']);
+
+        $dt = $_POST['datetime'];
+        $loc = $_POST['location'];
+        $mt = $_POST['meeting_type'];
+        $ft = $_POST['file_type'];
+        
+        writeData($dt, $loc, $mt, $ft, $upload_url);
+
     }
 ?>
 
@@ -35,7 +140,6 @@
     </select>
     <input type="file" name="fileData" accept=".pdf"/>
     <input type="submit" name="submit" value="submit" />
-
 </form>
 
 <style>
